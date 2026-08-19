9906f7706498682a2d6a869759cfd5f3396bc46f
Gabriel Saquette <114115521+gabrielsaquette@users.noreply.github.com>
2026-08-19 15:52:45 -0300

    Add files via upload

---
+++ modelos.js
@@ -0,0 +1,12 @@
+
+export const MODELOS_RAPIDOS = [
+  {
+    id: 'daily',
+    rotulo: 'Daily',
+    tipo: '1',
+    categoria: 6,
+    setor: 3,
+    valor: '',
+    descricao: 'Daily - 08h00',
+  },
+];
